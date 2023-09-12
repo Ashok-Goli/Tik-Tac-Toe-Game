d8862c3b0943219ed76e3b785bbfef7555669850
Ashok Goli <goliashok645@gmail.com>
2023-09-12 13:00:17 +0530

    adding math draw feature

--- index.js
+++ index.js
@@ -13,80 +13,96 @@ var player2 = document.getElementById("player2")
 
 var sign = "X"
 
-function selectedBox(id){
+function selectedBox(id) {
     var selectedElement = document.getElementById(id)
-    if(sign=="X"){
+    if (sign == "X") {
         player2.style.textShadow = "5px 5px 10px red, -5px -5px 10px red"
         player1.style.textShadow = "none"
         selectedElement.style.backgroundColor = "blue"
         selectedElement.style.boxShadow = "2px 2px 5px blue, -2px -2px 5px blue"
         selectedElement.style.border = "1px solid skyblue"
-    }else{
+        selectedElement.style.textShadow = "2px 2px 4px red, -2px -2px 4px red"
+
+    } else {
         player1.style.textShadow = "5px 5px 10px blue, -5px -5px 10px blue"
         player2.style.textShadow = "none"
         selectedElement.style.backgroundColor = "red"
         selectedElement.style.boxShadow = "2px 2px 5px red, -2px -2px 5px red"
         selectedElement.style.border = "1px solid orange"
+        selectedElement.style.textShadow = "2px 2px 4px blue, -2px -2px 4px blue"
     }
-    if(selectedElement.innerText == ""){
+    if (selectedElement.innerText == "") {
         selectedElement.innerText = sign
-
-        setTimeout(function(){
+        setTimeout(function () {
             winner(sign)
-        },100)
-        
-        setTimeout(function(){
-            if(sign == "X"){
-                sign="O"
-            }else{
-                sign="X"
+        }, 100)
+
+        setTimeout(function () {
+            if (sign == "X") {
+                sign = "O"
+            } else {
+                sign = "X"
             }
-        },100)
+        }, 100)
     }
 }
 
 //Winner Logic
-function winner(sign){
-    if((box1.innerText == sign && box2.innerText == sign && box3.innerText == sign) || (box4.innerText == sign && box5.innerText == sign && box6.innerText == sign) || (box7.innerText == sign && box8.innerText == sign && box9.innerText == sign) || (box1.innerText == sign && box4.innerText == sign && box7.innerText == sign) || (box2.innerText == sign && box5.innerText == sign && box8.innerText == sign) || (box3.innerText == sign && box6.innerText == sign && box9.innerText == sign) || (box1.innerText == sign && box5.innerText == sign && box9.innerText == sign) || (box3.innerText == sign && box5.innerText == sign && box7.innerText == sign)){
-        if(sign == "X"){
+function winner(sign) {
+    if ((box1.innerText == sign && box2.innerText == sign && box3.innerText == sign) || (box4.innerText == sign && box5.innerText == sign && box6.innerText == sign) || (box7.innerText == sign && box8.innerText == sign && box9.innerText == sign) || (box1.innerText == sign && box4.innerText == sign && box7.innerText == sign) || (box2.innerText == sign && box5.innerText == sign && box8.innerText == sign) || (box3.innerText == sign && box6.innerText == sign && box9.innerText == sign) || (box1.innerText == sign && box5.innerText == sign && box9.innerText == sign) || (box3.innerText == sign && box5.innerText == sign && box7.innerText == sign)) {
+        if (sign == "X") {
             player1.className = "fa-solid fa-user-graduate"
             player1.style.textShadow = "5px 5px 10px blue, -5px -5px 10px blue"
             player2.className = "fa-solid fa-user-injured"
             player2.style.textShadow = "none"
             document.getElementById("1").innerText = "WINNER"
-            document.getElementById("1").style.fontSize = "50px"
             count = 3
             var timer = document.getElementById("timer")
-            var id = setInterval(function(){
-                if(count<=0){
+            var id = setInterval(function () {
+                if (count <= 0) {
                     clearInterval(id)
                 }
-                timer.innerText=count
-                count-=1
-            },1000)
-            setTimeout(function(){
+                timer.innerText = count
+                count -= 1
+            }, 1000)
+            setTimeout(function () {
                 location.reload()
-            },4000)
-        }else{
+            }, 4000)
+        }
+        else {
             player2.className = "fa-solid fa-user-graduate"
             player2.style.textShadow = "5px 5px 10px red, -5px -5px 10px red"
             player1.className = "fa-solid fa-user-injured"
             player1.style.textShadow = "none"
             document.getElementById("2").innerText = "WINNER"
-            document.getElementById("2").style.fontSize = "50px"
             document.getElementById("2").style.color = "red"
             count = 3
             var timer = document.getElementById("timer")
-            var id = setInterval(function(){
-                if(count<=0){
+            var id = setInterval(function () {
+                if (count <= 0) {
                     clearInterval(id)
                 }
-                timer.innerText=count
-                count-=1
-            },1000)
-            setTimeout(function(){
+                timer.innerText = count
+                count -= 1
+            }, 1000)
+            setTimeout(function () {
                 location.reload()
-            },4000)
+            }, 4000)
         }
+    } else if (box1.innerText != "" && box2.innerText != "" && box3.innerText != "" && box4.innerText != "" && box5.innerText != "" && box6.innerText != "" && box7.innerText != "" && box8.innerText != "" && box9.innerText != "") {
+        var timer = document.getElementById("timer")
+        timer.innerText = "MATCH DRAW"
+        count = 3
+        
+        var id = setInterval(function () {
+            if (count <= 0) {
+                clearInterval(id)
+            }
+            timer.innerText = count
+            count -= 1
+        }, 1000)
+        setTimeout(function () {
+            location.reload()
+        }, 4000)
     }
 }
